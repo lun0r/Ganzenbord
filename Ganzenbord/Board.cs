f1cf875ca5360ef9c5a3d1c0ceb39211cfbabebc
PieterCorp <pieter@email.com>
2021-02-08 19:14:10 +0100

    kapot?

---
+++ Ganzenbord/Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ganzenbord
+{
+    internal class Board
+    {
+        public Field[,] BoardGrid { get; set; }
+
+        public Board()
+        {
+            BoardGrid = new Field[8, 8];
+        }
+    }
+}
