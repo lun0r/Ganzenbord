be2f031d9814fc443308710d7849c7030c1726b1
PieterCorp <pieter@email.com>
2021-02-10 14:50:08 +0100

    Binding

--- Ganzenbord/UserInteractionWindow.cs
+++ Ganzenbord/UserInteractionWindow.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Ganzenbord
 {
-    internal class UserInteractionWindow
+    public class UserInteractionWindow : INotifyPropertyChanged
     {
+        private int _boundNumber;
+
+        public int BoundNumber
+        {
+            get { return _boundNumber; }
+            set
+            {
+                if (_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        static private UserInteractionWindow userInteractionWindow;
+
         public UserInteractionWindow()
         {
+            BoundNumber = 50;
+        }
+
+        static public UserInteractionWindow GetUserInteractionWindow()
+        {
+            if (userInteractionWindow == null)
+            {
+                userInteractionWindow = new UserInteractionWindow();
+            }
+            return userInteractionWindow;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
