d699b545c606a5c7cd2358bd2f4abc0664744ebc
Dries Maes <dm.inbox@outlook.com>
2021-02-12 15:07:53 +0100

    pionnen in wrap panel

--- Ganzenbord/Fields/IField.cs
+++ Ganzenbord/Fields/IField.cs
@@ -4,7 +4,16 @@ namespace Ganzenbord
 {
     public interface IField
     {
+        Image Red { get; set; }
+        Image Purple { get; set; }
+        Image Green { get; set; }
+        Image Blue { get; set; }
+        Image Yellow { get; set; }
+        Image Orange { get; set; }
+        WrapPanel PawnWrap { get; set; }
+
         Image Background { get; set; }
+
         Label FieldNumber { get; set; }
         Image GamePiece { get; set; }
         Grid Grid { get; set; }
