a3cfa867c6d2400fc6b8f22a70977574c3385d7c
Dries Maes <dm.inbox@outlook.com>
2021-02-14 17:27:27 +0100

    Test voor Kobe

---
+++ Ganzenbord.UnitTest/DiceTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Ganzenbord;
+
+namespace Ganzenbord.UnitTest
+{
+    internal class DiceTest
+    {
+        private Dice _dice;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dice = new Dice();
+        }
+
+        [Test]
+        public void DiceRoll_WhenCalledUpon_ProvidesRandom()
+        {
+            int result = _dice.Roll();
+
+            Assert.That(result <= 6 || result >= 1);
+        }
+    }
+}
