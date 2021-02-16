a9729474947f2dcec0adb1bb334786aa4d028bba
Kobe Delobelle <delobellekobe@gmail.com>
2021-02-16 11:49:22 +0100

    merge

--- Ganzenbord.UnitTest/FieldsTests/DeathTest.cs
+++ Ganzenbord.UnitTest/FieldsTests/DeathTest.cs
@@ -14,20 +14,18 @@ namespace Ganzenbord.UnitTest
             _player = new Player("", "", PawnColor.BLUE);
             _death = new Death(1, 1, 1);
         }
-        [TestCase(1, 1, false, 10, 20)]
-        public void Method_WhenCalledUpon_ExpectedResult(int dice1, int dice2, bool isReversed, int currentBoardPosition, int expectedResult)
+        [TestCase()]
+        public void Method_WhenCalledUpon_ExpectedResult()
         {
             //arrange
-            _player.Dice1 = dice1;
-            _player.Dice2 = dice2;
-            _player.CurrentBoardPosition = currentBoardPosition;
-            _player.IsReversed = isReversed;
-
+            _player.HasDied = false;
+            
             //act
             int result = _death.ReturnMove(_player);
+            
 
             //assert
-            Assert.That(result == expectedResult);
+            Assert.That(result == 0 || _player.HasDied == true);
         }
     }
 }
